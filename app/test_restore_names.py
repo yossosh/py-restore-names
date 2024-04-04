3e4625f56b7e4b8527c981b26bff0487961d3740
Sofiia <sonagm9@gmail.com>
2024-04-04 17:05:31 +0300

    'Solution1'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,62 @@
-import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_names_with_missing_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_restore_names_with_existing_first_name() -> None:
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "full_name": "Jane Smith",
+        },
+    ]
+    restore_names(users)
+    assert users == [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "John Doe",
+        },
+        {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "full_name": "Jane Smith",
+        },
+    ]
+
+
+def test_restore_names_with_empty_users_list() -> None:
+    users = []
+    restore_names(users)
+    assert users == []
